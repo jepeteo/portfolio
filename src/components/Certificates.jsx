e318eb9e481cdc3b52af891a06ea44ec8779a569
Theodore Mentis <th.mentis@gmail.com>
2024-07-18 17:20:39 +0300

    add certificates to navigation. css tweaks. added more certificates

--- src/components/Certificates.jsx
+++ src/components/Certificates.jsx
@@ -6,26 +6,28 @@ const Certificates = () => {
     return <div>No certificates available</div>
   }
 
-  const certs = myCertificates.map((cert) => {
-    return (
-      <li
-        className="w-auto col-span-1 flex flex-col justify-center border border-color-1 hover:border-neutral-400 hover:bg-slate-100 dark:hover:bg-slate-800 dark:hover:border-neutral-200 transition-all"
-        key={cert.certName}
-      >
-        <a
-          href={isURL(cert.certUrl) ? cert.certUrl : "#"}
-          className="flex items-center gap-2 p-4"
+  const certs = myCertificates
+    .filter((cert) => cert.certVisible != false)
+    .map((cert) => {
+      return (
+        <li
+          className="w-auto col-span-1 flex flex-col justify-center border border-color-1 hover:border-neutral-400 hover:bg-slate-100 dark:hover:bg-slate-800 dark:hover:border-neutral-200 transition-all"
+          key={cert.certName}
         >
-          <img
-            className="w-8"
-            src={`images/${cert.certImg}`}
-            alt={cert.certName}
-          />
-          {cert.certName}
-        </a>
-      </li>
-    )
-  })
+          <a
+            href={isURL(cert.certUrl) ? cert.certUrl : "#"}
+            className="flex items-center gap-2 p-2 md:p-4"
+          >
+            <img
+              className="w-8"
+              src={`images/${cert.certImg}`}
+              alt={cert.certName}
+            />
+            {cert.certName}
+          </a>
+        </li>
+      )
+    })
   return (
     <section className="container" id="certificates">
       <h2 className="text-5xl font-bold">Certificates</h2>
