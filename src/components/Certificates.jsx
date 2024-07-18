3708f2edc43a552e54059ff6b8d8888d912a274c
Theodore Mentis <th.mentis@gmail.com>
2024-07-18 17:01:32 +0300

    certificate css

--- src/components/Certificates.jsx
+++ src/components/Certificates.jsx
@@ -8,16 +8,31 @@ const Certificates = () => {
 
   const certs = myCertificates.map((cert) => {
     return (
-      <li key={cert.certName}>
-        <a href={isURL(cert.certUrl) ? cert.certUrl : "#"}>{cert.certName}</a>
+      <li
+        className="w-auto col-span-1 flex flex-col justify-center border border-color-1 hover:border-neutral-400 hover:bg-slate-100 dark:hover:bg-slate-800 dark:hover:border-neutral-200 transition-all"
+        key={cert.certName}
+      >
+        <a
+          href={isURL(cert.certUrl) ? cert.certUrl : "#"}
+          className="flex items-center gap-2 p-4"
+        >
+          <img
+            className="w-8"
+            src={`images/${cert.certImg}`}
+            alt={cert.certName}
+          />
+          {cert.certName}
+        </a>
       </li>
     )
   })
   return (
-    <>
-      <div>Certificates</div>
-      <ul>{certs}</ul>
-    </>
+    <section className="container" id="certificates">
+      <h2 className="text-5xl font-bold">Certificates</h2>
+      <ul className="grid my-8 gap-x-6 gap-y-4 md:grid-cols-2 xl:grid-cols-3">
+        {certs}
+      </ul>
+    </section>
   )
 }
 
