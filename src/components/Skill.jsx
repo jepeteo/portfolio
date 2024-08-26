a7c4bdade0cc2dc391f813e117db23b7b2a951ce
Theodore Mentis <th.mentis@gmail.com>
2024-08-26 20:04:53 +0300

    memo on Skills

--- src/components/Skill.jsx
+++ src/components/Skill.jsx
@@ -1,47 +1,55 @@
-import React, { useEffect, useState } from "react";
-import PropTypes from "prop-types";
-import mySkills from "../assets/mySkills.json";
-import { initPopovers } from 'flowbite';
+import React, { useEffect, useState, useMemo } from "react"
+import PropTypes from "prop-types"
+import mySkills from "../assets/mySkills.json"
+import { initPopovers } from "flowbite"
 
 // Initialize Flowbite popovers
 const initializeFlowbite = () => {
-  initPopovers();
-};
+  initPopovers()
+}
 
 export default function Skills({ category, animation }) {
-  const [hoveredSkill, setHoveredSkill] = useState(null);
+  const [hoveredSkill, setHoveredSkill] = useState(null)
 
   useEffect(() => {
-    initializeFlowbite();
-  }, []);
+    initializeFlowbite()
+  }, [])
 
-  const skillList = mySkills
-    .filter((skill) => skill.category === category)
-    .filter((skill) => skill.visible === true)
-    .map((skill) => (
-      <li key={skill.skillName} className="mySkill">
-        <button 
-          data-popover-target={`popover-${skill.skillName}`} 
-          type="button" 
-          className="skill grow px-3 py-2 text-center"
-        >
-          {skill.skillName}
-        </button>
-        <div 
-          id={`popover-${skill.skillName}`} 
-          role="tooltip" 
-          className="absolute z-10 invisible inline-block w-64 text-sm text-gray-900 transition-opacity duration-300 bg-white border border-gray-200 rounded-lg shadow-sm opacity-0 dark:text-gray-100 dark:border-gray-500 dark:bg-gray-800"
-        >
-          <div className="px-3 py-2 bg-gray-100 border-b border-gray-200 rounded-t-lg dark:border-gray-600 dark:bg-gray-700">
-            <h3 className="font-semibold text-gray-900 dark:text-white">{skill.skillName}</h3>
+  const memoSkills = useMemo(() => {
+    return mySkills
+      .filter((skill) => skill.category === category)
+      .filter((skill) => skill.visible === true)
+      .map((skill) => (
+        <li key={skill.skillName} className="mySkill">
+          <button
+            data-popover-target={`popover-${skill.skillName}`}
+            type="button"
+            className="skill grow px-3 py-2 text-center"
+          >
+            {skill.skillName}
+          </button>
+          <div
+            id={`popover-${skill.skillName}`}
+            role="tooltip"
+            className="absolute z-10 invisible inline-block w-64 text-sm text-gray-900 transition-opacity duration-300 bg-white border border-gray-200 rounded-lg shadow-sm opacity-0 dark:text-gray-100 dark:border-gray-500 dark:bg-gray-800"
+          >
+            <div className="px-3 py-2 bg-gray-100 border-b border-gray-200 rounded-t-lg dark:border-gray-600 dark:bg-gray-700">
+              <h3 className="font-semibold text-gray-900 dark:text-white">
+                {skill.skillName}
+              </h3>
+            </div>
+            <div className="px-3 py-2">
+              <p>{skill.description}</p>
+            </div>
+            <div data-popper-arrow></div>
           </div>
-          <div className="px-3 py-2">
-            <p>{skill.description}</p>
-          </div>
-          <div data-popper-arrow></div>
-        </div>
-      </li>
-    ));
+        </li>
+      ))
+  }, [])
+
+  if (!memoSkills) {
+    return <div>No skills available</div>
+  }
 
   return (
     <div
@@ -49,18 +57,16 @@ export default function Skills({ category, animation }) {
     >
       <h3 className="text-2xl mt-4 text-center">{category}</h3>
       <ul className="list-none list-inside mt-4 flex flex-wrap gap-4">
-        {skillList}
+        {memoSkills}
       </ul>
     </div>
-  );
+  )
 }
 
 Skills.propTypes = {
   category: PropTypes.string,
   animation: PropTypes.string,
-};
-
-
+}
 
 //   const handleMouseEnter = (skill) => setHoveredSkill(skill)
 //   const handleMouseLeave = () => setHoveredSkill(null)
