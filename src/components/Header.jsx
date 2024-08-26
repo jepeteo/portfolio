b1f3963af82efbd167aad8d39a78859e14574f49
Theodore Mentis <th.mentis@gmail.com>
2024-08-26 20:15:29 +0300

    add projects to menu and fix css

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,7 @@ export default function Header({ toggleDarkMode, darkMode }) {
     <header className="border-b border-slate-700 sticky top-0 bg-slate-900/50 backdrop-blur-md z-10">
       <div className="container flex justify-between items-center py-2">
         <img src={logo} alt="logo" width="80px" className="p-2" />
-        <span className="mr-auto md:invisible xl:visible">
+        <span className="w-0 mr-auto md:invisible xl:visible xl:w-auto">
           Theodoros Mentis
         </span>
         <Nav toggleDarkMode={toggleDarkMode} darkMode={darkMode} />
