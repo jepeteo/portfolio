0999ec120bdd2be82c1022fa78301da20fafdc08
Theodore Mentis <th.mentis@gmail.com>
2024-08-26 19:57:42 +0300

    useMemo on Experience

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,26 +1,31 @@
+import React, { useMemo } from "react"
 import jobExperience from "../assets/jobExperience.json"
 import { nanoid } from "nanoid"
 
 export default function Experience() {
-  const jobs = jobExperience.map((job) => (
-    <div
-      key={nanoid(8)}
-      className="flex flex-col w-full md:flex-row justify-between py-4"
-    >
-      <div className="flex flex-col grow p-8 border shadow-lg scroll-appear">
-        <h3 className="text-xl font-bold mb-2">{job.title}</h3>
-        <p className="text-md font-light italic mb-2">
-          {job.company} || {job.from} - {job.to ? job.to : "Present"}
-        </p>
-        <p>{job.description}</p>
+  const memoJobs = useMemo(() => {
+    return jobExperience.map((job) => (
+      <div
+        key={nanoid(8)}
+        className="flex flex-col w-full md:flex-row justify-between py-4"
+      >
+        <div className="flex flex-col grow p-8 border shadow-lg scroll-appear">
+          <h3 className="text-xl font-bold mb-2">{job.title}</h3>
+          <p className="text-md font-light italic mb-2">
+            {job.company} || {job.from} - {job.to ? job.to : "Present"}
+          </p>
+          <p>{job.description}</p>
+        </div>
       </div>
-    </div>
-  ))
-
+    ))
+  }, [])
+  if (!memoJobs) {
+    return <div>No jobs available</div>
+  }
   return (
     <section className="container" id="experience">
       <h2 className="text-5xl font-bold ">Experience</h2>
-      {jobs}
+      {memoJobs}
     </section>
   )
 }
