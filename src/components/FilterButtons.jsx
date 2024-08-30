7bd70992db85475e6fa27986a7fb87f1e7e3adc9
Theodore Mentis <th.mentis@gmail.com>
2024-08-30 23:31:32 +0300

    fix project category Dynamic Site

--- src/components/FilterButtons.jsx
+++ src/components/FilterButtons.jsx
@@ -2,7 +2,7 @@ const FilterButtons = ({ setProjectType, setCurrentPage }) => {
   const filterTypes = [
     { type: null, label: "All" },
     { type: "App", label: "App" },
-    { type: "Blog", label: "Blog" },
+    // { type: "Blog", label: "Blog" },
     { type: "Dynamic Site", label: "Dynamic" },
     { type: "E-Shop", label: "E-Shop" },
     { type: "Static", label: "Static" },
