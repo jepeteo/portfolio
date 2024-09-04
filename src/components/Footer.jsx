56a1d7f7856491a49498b2ff87e5c703ed128cf0
Theodore Mentis <th.mentis@gmail.com>
2024-09-04 18:37:19 +0300

    fixed footer (center); added email; removed images;

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,29 +1,40 @@
 import react from "../assets/images/react.svg"
 import tailwind from "../assets/images/tailwindcss.svg"
-import github from "../assets/images/github.svg"
-import linkedin from "../assets/images/linkedin.svg"
+import { BsEnvelopeAt } from "react-icons/bs"
+import { BsLinkedin } from "react-icons/bs"
+import { BsGithub } from "react-icons/bs"
 
 export default function Footer() {
   return (
     <footer className="p-2 border-t border-slate-700	">
-      <div className="container flex p-0 justify-between items-center flex-col gap-4 md:flex-row">
-        <div>&copy; {new Date().getFullYear()} Theodore Mentis</div>
+      <div className="container grid grid-cols-1 p-0 justify-between items-center gap-4 md:flex-row md:grid-cols-3">
+        <div className="text-center md:text-left">
+          &copy; {new Date().getFullYear()} Theodore Mentis
+        </div>
         <div className="connect flex justify-center gap-2">
-          <a href="https://github.com/jepeteo" className="p-2 rounded-xl">
-            <img className="inline h-6" src={github} alt="Find me on Github" />
+          <a
+            href="https://github.com/jepeteo"
+            className="p-2 rounded-xl"
+            title="Find me on Github"
+          >
+            <BsGithub className="inline h-6 w-6 text-black" />
           </a>
           <a
             href="https://www.linkedin.com/in/thmentis/"
             className="p-2 rounded-xl"
+            title="View my profile on LinkedIn"
           >
-            <img
-              className="inline h-6"
-              src={linkedin}
-              alt="View my profile on LinkedIn"
-            />
+            <BsLinkedin className="inline h-6 w-6 text-black" />
+          </a>
+          <a
+            href="mailto:th.mentis@gmail.com"
+            className=" p-2 rounded-xl"
+            title="Send me an email"
+          >
+            <BsEnvelopeAt className="inline h-6 w-6 text-black" />
           </a>
         </div>
-        <div>
+        <div className="text-center md:text-right">
           Made with React{" "}
           <img className="inline h-6" src={react} alt="React Logo" /> & Tailwind
           CSS{" "}
