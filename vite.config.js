3d09f5d945ebd26c2b48abc3ab4670b813357f5a
Theodore Mentis <th.mentis@gmail.com>
2024-09-08 18:18:51 +0300

    optimize vite config - manualChunks

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio/',
+  base: "/portfolio/",
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ["react", "react-dom"],
+          vendor: ["nanoid", "react-typed", "flowbite-react"],
+        },
+      },
+    },
+  },
 })
